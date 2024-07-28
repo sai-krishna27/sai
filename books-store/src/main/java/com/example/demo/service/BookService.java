dd9ec11c08bd7890f7a501b2c112f125b4864837
sai-krishna27 <132563001+sai-krishna27@users.noreply.github.com>
2024-07-28 22:15:05 +0530

    Update BookService.java

--- books-store/src/main/java/com/example/demo/service/BookService.java
+++ books-store/src/main/java/com/example/demo/service/BookService.java
@@ -1,12 +1,18 @@
 package com.example.demo.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Entity.Book;
 import com.example.demo.repo.BookRepo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BookService {
