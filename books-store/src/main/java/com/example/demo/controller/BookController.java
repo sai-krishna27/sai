5ddf3ebf59076f867c2cef8ffefc24e2d139048f
sai-krishna27 <132563001+sai-krishna27@users.noreply.github.com>
2024-07-28 22:13:52 +0530

    Update BookController.java

--- books-store/src/main/java/com/example/demo/controller/BookController.java
+++ books-store/src/main/java/com/example/demo/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,6 +57,11 @@ public class BookController {
 		ObjectMapper mapper=new ObjectMapper();
 		return mapper.writeValueAsString(dao.getBookById(id));
 		
+	}
+
+	@PatchMapping("/books")
+	public Book patialUpdateBook(@RequestBody String book) throws JsonMappingException, JsonProcessingException {
+		return dao.updateBook(book);
 	}
 	
 	@PutMapping("/books")
